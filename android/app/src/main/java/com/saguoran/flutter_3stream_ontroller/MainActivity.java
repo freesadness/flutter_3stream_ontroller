0d5cb45c761f3f870bf8b455c33bbeaecfb4f688
freesadness <freesadness1226@gmail.com>
2019-08-26 14:56:03 -0400

    Initial commit

---
+++ android/app/src/main/java/com/saguoran/flutter_3stream_ontroller/MainActivity.java
@@ -0,0 +1,13 @@
+package com.saguoran.flutter_3stream_ontroller;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
